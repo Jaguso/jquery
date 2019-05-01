cd6d8f23f5bed8321bb4ff921af6e4bc9a3a6fe7
Jaguso <jagutzo@gmail.com>
2019-04-30 20:10:40 -0500

    jquery setup made

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("button").addClass("animated bounce");
+  $(".well").addClass("animated shake");
+  $("#target3").addClass("animated fadeOut");
+});
+
+$(document).ready(function() {
+  $(".name").css("color", "blue");
+  console.log('working')
+})
